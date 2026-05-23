36421f9e08d4bcb3c864eb4d8473100274c0e3ce
Ajax9922 <ajaysheria90@gmail.com>
2026-05-23 03:09:40 -0600

    Update lcc.js

--- lcc.js
+++ lcc.js
@@ -1,16 +1,3 @@
-// ── META PIXELS ─────────────────────────────────────────────────────────────
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1242574784615190');
-fbq('init', '1292671809160921');
-fbq('track', 'PageView');
-
 // ── LCC MAIN ─────────────────────────────────────────────────────────────────
 (function() {
 
